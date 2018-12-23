f5e929b945a9f2e9ce66dfff5e5da2703c80ac34
mrgoodrich <mryangoodrich@icloud.com>
2018-12-23 01:18:52 -0700

    📍 Chronal Coordinates 🎯 - 1: 2042, 2: 1754

--- D6/first.js
+++ D6/first.js
@@ -3,17 +3,6 @@ String.prototype.replaceAll = function(search, replacement) {
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-// console.log("tes".replaceAll('te', 'f'));
-
-// for (var ndx in arr) {
-//   var val = arr[ndx];
-//
-//   console.log(val);
-// }
-
-// var map = {};
-// (!map.test) && (map.test = 'hi');
-
 var data = [
 
   // [1, 1],
